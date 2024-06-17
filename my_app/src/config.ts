e5a7c58dd3734c4d8c5c2700d84c79297eae69dd
KrishnaBhargavN <krishnabhargavn.221cs228@nitk.edu.in>
2024-06-17 17:52:09 +0530

    Small changes

--- my_app/src/config.ts
+++ my_app/src/config.ts
@@ -1,2 +1,4 @@
 const BACKEND_URL = "https://backend.bhargavnkrishna.workers.dev";
+const jwt_token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjczOGYyMjRhLTJjOWUtNGI3YS04ZDA5LWI4Y2NhZTVhZDk2YyJ9.e4AM5KxVvmR1WRYmsr06PQyQH-iyg0uVpsF8TVR8T9U"; // bearer token
 export { BACKEND_URL };
