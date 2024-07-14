4d3ac730ce30338a50421e8ef94356f0dd634d33
Vatsal Gandhi <139644618+vg239@users.noreply.github.com>
2024-07-14 21:37:55 +0530

    Update useBlogs.ts
    Used react in order to initially fetch the data from the URL by using the jwt token for authorization 
    Nextly added a try catch block in order to catch error if any
    Made the Blog Array which would contain the data which is fetched from the website
    And then once the website is mount use getBlogs()
    the function useBlogs() returns the Blog array which contains the Blogs

--- my_app/src/hooks/useBlogs.ts
+++ my_app/src/hooks/useBlogs.ts
@@ -1,11 +1,28 @@
 import axios from "axios";
 import { BACKEND_URL } from "../config";
+import {jwt_token} from "../config";
+import { useState, useEffect } from "react";
 
-export default function useBlogs() {
-  const createUser = async (name: string, job: string) => {
-    const response = await axios.get(`${BACKEND_URL}/api/v1/blog/bulk`);
-    return response.data.blogs;
-  };
+export default function useBlogs(){
+  const [blogArray, setBlogArray] = useState();
 
-  return { createUser };
+  const getBlogs = async()=>{
+    try{
+    const response = await axios.get(`${BACKEND_URL}/api/v1/blog/bulk`,{
+      headers : {
+        Authorization : `Bearer ${jwt_token}` 
+      }});
+    setBlogArray(response.data)
+  }
+  
+  catch(err){
+    console.log("error")
+  }
 }
+  useEffect(()=>{
+    getBlogs();
+  },[])
+
+  return blogArray
+
+};
